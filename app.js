eb202852baece175f77ce6bb32abfcf540737626
Quang Phan <oclockvn@gmail.com>
2021-06-14 16:27:26 +0700

    add business day add

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const btnGetDate = document.getElementById('btnGetDate');
 const fromDate = document.getElementById('fromDate');
 const toDate = document.getElementById('toDate');
 const diffDays = document.getElementById('diffDays');
-const untilDate = document.getElementById('untilDate');
+const fromDate2 = document.getElementById('fromDate2');
 
 const diffResult = document.getElementById('diffResult');
+const targetDateEle = document.getElementById('targetDate');
+const sqlEle = document.getElementById('sql');
 
 toDate.valueAsDate = new Date();
-untilDate.valueAsDate = new Date();
+fromDate2.valueAsDate = new Date();
 
 btnDayDiff.addEventListener('click', () => {
   const fromValue = fromDate.value;
@@ -23,28 +25,24 @@ btnDayDiff.addEventListener('click', () => {
   const from = moment(fromValue);
   const to = moment(toValue);
 
-  const dayDiff = from.diff(to, 'days');
-  const bizDiff = from.businessDiff(to);
+  const dayDiff = to.diff(from, 'days');
+  const bizDiff = to.businessDiff(from);
   console.log(`day diff: ${dayDiff}, business diff: ${bizDiff}`);
 
-  diffResult.innerHTML = `<b>Business diff = ${bizDiff}</b> (days diff = ${dayDiff})`;
+  diffResult.innerHTML = `Business day diff = ${bizDiff}, normal day diff = ${dayDiff}`;
 });
 
 btnGetDate.addEventListener('click', () => {
-  // const fromValue = fromDate.value;
-  // const toValue = toDate.value;
+  const fromValue = fromDate2.value;
+  const diff = Number(diffDays.value) || 0;
 
-  // if (!fromValue || !toValue) {
-  //   diffResult.innerHTML = '';
-  //   return;
-  // }
+  if (!fromValue) {
+    dateResult.innerHTML = '';
+    return;
+  }
 
-  // const from = moment(fromValue);
-  // const to = moment(toValue);
+  const from = moment(fromValue);
+  const to = from.businessAdd(diff);
 
-  // const dayDiff = from.diff(to, 'days');
-  // const bizDiff = from.businessDiff(to);
-  // console.log(`day diff: ${dayDiff}, business diff: ${bizDiff}`);
-
-  // diffResult.innerHTML = `<b>Business diff = ${bizDiff}</b> (days diff = ${dayDiff})`;
+  targetDateEle.innerText = to.format('MM/DD/YYYY');
 });
